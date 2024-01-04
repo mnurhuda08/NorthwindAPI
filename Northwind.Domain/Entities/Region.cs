c523168b80dc52e1d98a095d09d87cbd2419ac91
mnurhuda08 <mnurhuda009@gmail.com>
2024-01-04 13:33:26 +0700

    Add Layer Domain

---
+++ Northwind.Domain/Entities/Region.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Entities
+{
+    [Table("Region")]
+    public class Region
+    {
+        [Key]
+        public int RegionId { get; set; }
+
+        public string RegionDescription { get; set; }
+    }
+}
---
+++ Northwind.Domain/Repositories/IRegionRepository.cs
@@ -0,0 +1,24 @@
+﻿using Northwind.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Domain.Repositories
+{
+    internal interface IRegionRepository
+    {
+        IEnumerable<Region> FindAllRegion();
+
+        Task<IEnumerable<Region>> FindAllRegionsAsync();
+
+        Region FindRegionByID(int id);
+
+        void Insert(Region region);
+
+        void Edit(Region region);
+
+        void Remove(Region region);
+    }
+}
